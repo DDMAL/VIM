ebd2ce1c026151aad285b88d4d36e1c21e51f59f
Zhanna Klimanova <zklimanova@yahoo.com>
2023-07-31 08:03:27 -0400

    Add new template configurations for django and nginx

--- django/VIM/settings.py
+++ django/VIM/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0imv3dwuz0%pg13g72upze7k#ap^^8=ra=uj#i@ejpxqf@gk@0"
+SECRET_KEY = "django-insecure-$s!(+krcak0g&ic13e&aieb&xv#4u7fi=g99^ff&s^w3ouw13d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +73,12 @@ WSGI_APPLICATION = "VIM.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.sqlite3",
-#        "NAME": BASE_DIR / "db.sqlite3",
-#    }
-#}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 # Password validation
