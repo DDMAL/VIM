196bfdb4ca4f7945e87ff99296fb9fdd0cf0edcc
Dylan Hillerbrand <dylanhillerbrand@locals-mac-mini.mynetworksettings.com>
2023-09-13 16:44:27 -0400

    Add postgres and docker compose

--- web-app/django/VIM/settings.py
+++ web-app/django/VIM/settings.py
@@ -11,13 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-0imv3dwuz0%pg13g72upze7k#ap^^8=ra=uj#i@ejpxqf@gk@0"
@@ -69,16 +66,16 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "VIM.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-#DATABASES = {
-#    "default": {
-#        "ENGINE": "django.db.backends.sqlite3",
-#        "NAME": BASE_DIR / "db.sqlite3",
-#    }
-#}
+DATABASES = {
+   "default": {
+       "ENGINE": "django.db.backends.postgresql",
+       "NAME": os.environ.get("POSTGRES_DB"),
+       "USER": os.environ.get("POSTGRES_USER"),
+       "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+       "HOST": "vim-db",
+       "PORT": "5432",
+   }
+}
 
 
 # Password validation
