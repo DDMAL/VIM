450069570fd10dd62bafa5ea45386e5427e88661
Dylan Hillerbrand <dhillerbrand@gmail.com>
2023-09-19 17:02:07 -0400

    Add initial non-user models

---
+++ web-app/django/VIM/apps/vim/models/__init__.py
@@ -0,0 +1,4 @@
+from VIM.apps.vim.models.instrument import Instrument
+from VIM.apps.vim.models.instrument_name import InstrumentName
+from VIM.apps.vim.models.language import Language
+from VIM.apps.vim.models.av_resource import AVResource
