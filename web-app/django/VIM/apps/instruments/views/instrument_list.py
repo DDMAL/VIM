ad70da71d2eb085b6525bffd9b8d7fc553c38e52
Yinan Zhou <yinan.a.zhou@gmail.com>
2023-09-22 15:12:07 -0400

    🆕 Initialize templates

--- web-app/django/VIM/apps/instruments/views/instrument_list.py
+++ web-app/django/VIM/apps/instruments/views/instrument_list.py
@@ -3,6 +3,6 @@ from VIM.apps.instruments.models import Instrument
 
 
 class InstrumentList(ListView):
-    template_name = "instrument_list.html"
+    template_name = "instruments/index.html"
     context_object_name = "instruments"
     model = Instrument
