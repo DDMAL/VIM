f6dc45ab8e0194129e73b9ab70cd079342afd694
Dylan Hillerbrand <dhillerbrand@gmail.com>
2023-09-26 14:45:50 -0400

    Add simple paginator

--- web-app/django/VIM/apps/instruments/views/instrument_list.py
+++ web-app/django/VIM/apps/instruments/views/instrument_list.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.views.generic import ListView
 from VIM.apps.instruments.models import Instrument
 
@@ -5,3 +6,9 @@ class InstrumentList(ListView):
     template_name = "instruments/index.html"
     context_object_name = "instruments"
     model = Instrument
+
+    def get_paginate_by(self, queryset):
+        paginate_by = self.request.GET.get("paginate_by", None)
+        if paginate_by is not None:
+            return paginate_by
+        return 20
