11129b20aaccacb26c05db7a7ab537888bec9574
Dylan Hillerbrand <dhillerbrand@gmail.com>
2023-09-28 10:58:32 -0400

    Update documentation for import_languages command

--- web-app/django/VIM/apps/instruments/management/commands/import_languages.py
+++ web-app/django/VIM/apps/instruments/management/commands/import_languages.py
@@ -3,7 +3,14 @@ from VIM.apps.instruments.models import Language
 
 
 class Command(BaseCommand):
-    help = "Imports language objects"
+    """
+    The import_languages command populates the database with languages in which instrument
+    names can be provided in VIM.
+    
+    NOTE: For now, this script only imports English and French.
+    """
+    
+    help = "Imports possible languages for instrument names from Wikidata."
 
     def handle(self, *args, **options):
         Language.objects.create(
