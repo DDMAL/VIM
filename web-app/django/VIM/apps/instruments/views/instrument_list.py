718f173f219e73fcf9e6c0ee4abe2e0fd4b88e92
Dylan Hillerbrand <dhillerbrand@gmail.com>
2023-09-28 09:24:37 -0400

    Add pagination to instrument list view

--- web-app/django/VIM/apps/instruments/views/instrument_list.py
+++ web-app/django/VIM/apps/instruments/views/instrument_list.py
@@ -1,8 +1,21 @@
 from django.views.generic import ListView
 from VIM.apps.instruments.models import Instrument
 
-
 class InstrumentList(ListView):
+    """
+    Provides a paginated list of all instruments in the database.
+
+    Pass `page` and `paginate_by` as query parameters to control pagination.
+    Defaults to 20 instruments per page.
+    """
     template_name = "instrument_list.html"
     context_object_name = "instruments"
     model = Instrument
+
+    def get_paginate_by(self, queryset) -> int:
+        pag_by_param: str = self.request.GET.get("paginate_by", "20")
+        try:
+            paginate_by = int(pag_by_param)
+        except ValueError:
+            paginate_by = 20
+        return paginate_by
