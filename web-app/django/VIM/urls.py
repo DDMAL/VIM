04936e1802fc8489c86126c1ae3658b010d0a3ef
Yinan Zhou <yinan.a.zhou@gmail.com>
2023-10-03 16:23:59 -0400

    Add home page

--- web-app/django/VIM/urls.py
+++ web-app/django/VIM/urls.py
@@ -20,6 +20,7 @@ from django.urls import include
 from VIM.apps.instruments.views.instrument_list import InstrumentList
 
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("", include("VIM.apps.main.urls", namespace="main")),
     path("instruments/", InstrumentList.as_view(), name="instrument-list"),
