b7218d49197c0b0294f452cf2968c9e6e4690a5e
Yinan Zhou <yinan.a.zhou@gmail.com>
2023-10-03 16:23:59 -0400

    Add home page

--- web-app/django/VIM/settings.py
+++ web-app/django/VIM/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "VIM.apps.main",
     "VIM.apps.instruments",
 ]
 
@@ -120,9 +121,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static"
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
