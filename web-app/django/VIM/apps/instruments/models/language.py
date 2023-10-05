3bd6d4e89eac30308473d8bba4710955db0fcbd4
Yinan Zhou <yinan.a.zhou@gmail.com>
2023-10-05 12:05:10 -0400

    Reformat py files

--- web-app/django/VIM/apps/instruments/models/language.py
+++ web-app/django/VIM/apps/instruments/models/language.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Language(models.Model):
     wikidata_code = models.CharField(max_length=10, unique=True, blank=False)
-    wikidata_id = models.CharField(max_length=20,unique=True)
+    wikidata_id = models.CharField(max_length=20, unique=True)
     en_label = models.CharField(
         max_length=50, blank=False, help_text="Language label in English"
     )
