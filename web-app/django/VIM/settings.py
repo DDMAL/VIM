c87c526f0f85ac404023d59a544ecea9ddd0880a
Dylan Hillerbrand <dhillerbrand@gmail.com>
2023-10-11 14:08:31 -0400

    Serve static files with nginx

--- web-app/django/VIM/settings.py
+++ web-app/django/VIM/settings.py
@@ -131,9 +131,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_ROOT = "/virtual-instrument-museum/static"
 STATIC_URL = "static/"
-
-STATICFILES_DIRS = [BASE_DIR / "VIM" / "static"]
+STATICFILES_DIRS = [
+    BASE_DIR / "VIM" / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
