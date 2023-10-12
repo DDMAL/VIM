f1c73a8b8213c4351ed075599635dfcab46a82dd
Yinan Zhou <yinan.a.zhou@gmail.com>
2023-10-12 12:35:33 -0400

    Temporarily remove list view

--- web-app/django/VIM/apps/instruments/static/instruments/js/DisplayMode.js
+++ web-app/django/VIM/apps/instruments/static/instruments/js/DisplayMode.js
@@ -1,10 +1,10 @@
 
 const masonryBtn = document.getElementById("masonry-btn");
-const listBtn = document.getElementById("list-btn");
+// const listBtn = document.getElementById("list-btn");
 const stdBtn = document.getElementById("std-btn");
 
 const masonryView = document.getElementById('masonry-view');
-const listView = document.getElementById('list-view');
+// const listView = document.getElementById('list-view');
 const stdView = document.getElementById('std-view');
 
 updateDisplayMode();
@@ -23,26 +23,26 @@ function updateDisplayMode() {
         case 'masonry':
             setMasonryView();
             masonryBtn.style.display = "";
-            listBtn.style.display = "none";
+            // listBtn.style.display = "none";
             stdBtn.style.display = "none";
             masonryView.style.display = "";
-            listView.style.display = "none";
+            // listView.style.display = "none";
             stdView.style.display = "none";
             break;
-        case 'list':
-            masonryBtn.style.display = "none";
-            listBtn.style.display = "";
-            stdBtn.style.display = "none";
-            masonryView.style.display = "none";
-            listView.style.display = "";
-            stdView.style.display = "none";
-            break;
+        // case 'list':
+        //     masonryBtn.style.display = "none";
+        //     listBtn.style.display = "";
+        //     stdBtn.style.display = "none";
+        //     masonryView.style.display = "none";
+        //     listView.style.display = "";
+        //     stdView.style.display = "none";
+        //     break;
         case 'standard':
             masonryBtn.style.display = "none";
-            listBtn.style.display = "none";
+            // listBtn.style.display = "none";
             stdBtn.style.display = "";
             masonryView.style.display = "none";
-            listView.style.display = "none";
+            // listView.style.display = "none";
             stdView.style.display = "";
             break;
         default:
@@ -52,14 +52,14 @@ function updateDisplayMode() {
 
 // Switch to the next mode
 masonryBtn.addEventListener("click", () => {
-    setDisplayMode('list');
-    updateDisplayMode();
-});
-
-listBtn.addEventListener("click", () => {
     setDisplayMode('standard');
     updateDisplayMode();
 });
+
+// listBtn.addEventListener("click", () => {
+//     setDisplayMode('standard');
+//     updateDisplayMode();
+// });
 
 stdBtn.addEventListener("click", () => {
     setDisplayMode('masonry'); 
