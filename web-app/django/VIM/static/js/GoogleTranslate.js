34155253e591eaee9dccc3d47b11849d6eb47c39
Yinan Zhou <yinan.a.zhou@gmail.com>
2023-10-16 17:37:04 -0400

    Add instruction notes for language button diff

--- web-app/django/VIM/static/js/GoogleTranslate.js
+++ web-app/django/VIM/static/js/GoogleTranslate.js
@@ -13,3 +13,22 @@ function googleTranslateElementInit() {
     const grandparent = parent.parentElement;
     grandparent.classList.add('h-100', 'd-flex', 'align-items-center');
 } 
+
+const pageLanguage = document.querySelector("#google_translate_element");
+const pageBadge = document.querySelector("#page-language-badge");
+
+updatePageBadge();
+
+function updatePageBadge() {
+    const hidePageBadge = localStorage.getItem('hidePageBadge') || false;
+    if (hidePageBadge) {
+        pageBadge.style.display = "none";
+    } else {
+        pageBadge.style.display = "";
+    }
+}
+
+pageLanguage.addEventListener("click", function () {
+    localStorage.setItem("hidePageBadge", true);
+    updatePageBadge();
+});
