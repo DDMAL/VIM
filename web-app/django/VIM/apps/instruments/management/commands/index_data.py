051291585ad46d9ba1039fa70b972af0dc3b0f3f
Dylan Hillerbrand <dhillerbrand@gmail.com>
2023-10-20 15:42:43 -0400

    Hardcode French HBS names & display names based on language

--- web-app/django/VIM/apps/instruments/management/commands/index_data.py
+++ web-app/django/VIM/apps/instruments/management/commands/index_data.py
@@ -19,21 +19,20 @@ class Command(BaseCommand):
                 wikidata_id_s=F("wikidata_id"),
                 hornbostel_sachs_class_s=F("hornbostel_sachs_class"),
                 hbs_prim_cat_s=Left(F("hornbostel_sachs_class"), 1),
-                hbs_sec_cat_s=Left(F("hornbostel_sachs_class"), 2),
                 mimo_class_s=F("mimo_class"),
                 type=V("instrument"),
             )
         )
         hbs_label_map = self.build_hbs_label_map()
         for instrument in instruments:
-            instrument["hbs_prim_cat_label_s"] = (
-                hbs_label_map[instrument["hbs_prim_cat_s"]]
+            instrument["hbs_prim_cat_label_en_s"] = (
+                hbs_label_map["en"][instrument["hbs_prim_cat_s"]]
                 if instrument["hbs_prim_cat_s"] != ""
                 else ""
             )
-            instrument["hbs_sec_cat_label_s"] = (
-                hbs_label_map[instrument["hbs_sec_cat_s"]]
-                if instrument["hbs_sec_cat_s"] != ""
+            instrument["hbs_prim_cat_label_fr_s"] = (
+                hbs_label_map["fr"][instrument["hbs_prim_cat_s"]]
+                if instrument["hbs_prim_cat_s"] != ""
                 else ""
             )
         requests.post(
@@ -44,16 +43,34 @@ class Command(BaseCommand):
         )
 
     def build_hbs_label_map(self):
-        top_concepts = requests.get(
-            "https://vocabulary.mimo-international.com/rest/v1/HornbostelAndSachs/topConcepts"
-        ).json()["topconcepts"]
-        hbs_label_map = {}
-        for t_c in top_concepts:
-            hbs_label_map[t_c["notation"]] = t_c["label"]
-            child_concepts = requests.get(
-                "https://vocabulary.mimo-international.com/rest/v1/HornbostelAndSachs/children?uri="
-                + t_c["uri"]
-            ).json()["narrower"]
-            for c_c in child_concepts:
-                hbs_label_map[c_c["notation"]] = c_c["prefLabel"]
+        # For now, we just want the names in English and French of the first category of
+        # the Hornbostel-Sachs classification.
+        eng_name_mapping = {
+            "1": "Idiophones",
+            "2": "Membranophones",
+            "3": "Chordophones",
+            "4": "Aerophones",
+            "5": "Electrophones",
+        }
+        fr_name_mapping = {
+            "1": "Idiophones",
+            "2": "Membranophones",
+            "3": "Cordophones",
+            "4": "Aérophones",
+            "5": "Électrophones",
+        }
+        hbs_label_map = {"en": eng_name_mapping, "fr": fr_name_mapping}
         return hbs_label_map
+        # top_concepts = requests.get(
+        #     "https://vocabulary.mimo-international.com/rest/v1/HornbostelAndSachs/topConcepts"
+        # ).json()["topconcepts"]
+        # hbs_label_map = {}
+        # for t_c in top_concepts:
+        #     hbs_label_map[t_c["notation"]] = t_c["label"]
+        #     child_concepts = requests.get(
+        #         "https://vocabulary.mimo-international.com/rest/v1/HornbostelAndSachs/children?uri="
+        #         + t_c["uri"]
+        #     ).json()["narrower"]
+        #     for c_c in child_concepts:
+        #         hbs_label_map[c_c["notation"]] = c_c["prefLabel"]
+        # return hbs_label_map
