12cfe5a56a7f5b86942072285b610c76be7058cd
Karlie Fang <380923800@qq.com>
2023-10-30 03:50:49 +0800

    Reformat login function

--- web-app/django/VIM/apps/main/urls.py
+++ web-app/django/VIM/apps/main/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("login/", views.login_view, name="login"),
-    path('change_password/', views.change_password, name='change_password'),
-    path('logout/', views.user_logout, name='logout'),
+    path("change_password/", views.change_password, name="change_password"),
+    path("logout/", views.user_logout, name="logout"),
 ]
