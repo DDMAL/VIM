243c2dc39435e473c05940ad4282783c23f3f662
Karlie Fang <380923800@qq.com>
2023-10-31 01:52:56 +0800

    Use default views and forms in login, logout, change pwd and register

--- web-app/django/VIM/apps/main/urls.py
+++ web-app/django/VIM/apps/main/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = "main"
@@ -6,8 +7,21 @@ app_name = "main"
 urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
-    path("login/", views.user_login, name="login"),
-    path("change_password/", views.change_password, name="change_password"),
-    path("logout/", views.user_logout, name="logout"),
     path("register/", views.register, name="register"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(next_page="main:home"), name="logout"),
+    path(
+        "change-password/",
+        auth_views.PasswordChangeView.as_view(template_name="registration/change_password.html", success_url="/",),
+        name="change_password",
+    ),
+    path(
+        "accounts/login/", 
+        auth_views.LoginView.as_view(
+            template_name="registration/login.html",
+            redirect_authenticated_user=True,
+            redirect_field_name="next",
+            next_page="main:home",
+        ), 
+        name="login",
+    ),
 ]
--- web-app/django/VIM/apps/main/views.py
+++ web-app/django/VIM/apps/main/views.py
@@ -1,6 +1,7 @@
-from django.contrib.auth import login, logout, update_session_auth_hash
-from django.shortcuts import render, redirect
-from .forms import LoginForm, RegistrationForm, CustomPasswordChangeForm
+from django.contrib.auth import login
+from django.shortcuts import render
+from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
 
 
 def home(request):
@@ -11,43 +12,14 @@ def about(request):
     return render(request, "main/about.html", {"active_tab": "about"})
 
 
-def user_login(request):
-    if request.method == "POST":
-        form = LoginForm(request, request.POST)
-        if form.is_valid():
-            login(request, form.get_user())
-            return redirect("main:home")
-    else:
-        form = LoginForm()
-
-    return render(request, "registration/login.html", {"form": form})
-
-
-def user_logout(request):
-    logout(request)
-    return redirect("main:home")
-
-
 def register(request):
     if request.method == "POST":
-        form = RegistrationForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
             return redirect("main:home")
     else:
-        form = RegistrationForm()
+        form = UserCreationForm()
 
     return render(request, "registration/register.html", {"form": form})
-
-
-def change_password(request):
-    if request.method == "POST":
-        form = CustomPasswordChangeForm(request.user, request.POST)
-        if form.is_valid():
-            user = form.save()
-            update_session_auth_hash(request, user)
-            return redirect("main:home")
-    else:
-        form = CustomPasswordChangeForm(request.user)
-    return render(request, "registration/change_password.html", {"form": form})
