2d90b0a1e0eaac29d2c94e1f81aa2bd156cf643f
Karlie Fang <380923800@qq.com>
2023-10-31 01:56:45 +0800

    reformat urls.py

--- web-app/django/VIM/apps/main/urls.py
+++ web-app/django/VIM/apps/main/urls.py
@@ -8,20 +8,27 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("register/", views.register, name="register"),
-    path("accounts/logout/", auth_views.LogoutView.as_view(next_page="main:home"), name="logout"),
+    path(
+        "accounts/logout/", 
+        auth_views.LogoutView.as_view(next_page="main:home"), 
+        name="logout"
+    ),
     path(
         "change-password/",
-        auth_views.PasswordChangeView.as_view(template_name="registration/change_password.html", success_url="/",),
+        auth_views.PasswordChangeView.as_view(
+            template_name="registration/change_password.html", 
+            success_url="/",
+        ),
         name="change_password",
     ),
     path(
-        "accounts/login/", 
+        "accounts/login/",
         auth_views.LoginView.as_view(
             template_name="registration/login.html",
             redirect_authenticated_user=True,
             redirect_field_name="next",
             next_page="main:home",
-        ), 
+        ),
         name="login",
     ),
 ]
