de19c30e2cf928d3443be7c197acc5d0f333768c
Yinan Zhou <yinan.a.zhou@gmail.com>
2024-07-16 12:26:05 -0400

    style: add formatter for front end code
    - Set up djlint for html, prettier for js and css
    - Reformat front end code
    
    Refs: #113

--- web-app/django/VIM/apps/instruments/static/instruments/js/PaginationTools.js
+++ web-app/django/VIM/apps/instruments/static/instruments/js/PaginationTools.js
@@ -1,77 +1,77 @@
 const instrumentNumElement = document.getElementById('instrumentNum');
 const instrumentNum = instrumentNumElement.getAttribute('data-instrument-num');
-const pageNumInput = document.getElementById("page-num");
-const pageBtns = document.querySelectorAll(".page-link");
-const optionRadios = document.querySelectorAll(".option-radio");
+const pageNumInput = document.getElementById('page-num');
+const pageBtns = document.querySelectorAll('.page-link');
+const optionRadios = document.querySelectorAll('.option-radio');
 
 updatePaginationSetting();
 
 function updatePaginationSetting() {
-    const url = new URL(window.location.href);
-    const page = url.searchParams.get("page") || 1;
-    const paginateBy = url.searchParams.get("paginate_by") || 20;
-    setPage(page);
-    setPaginateBy(paginateBy);
-    pageNumInput.max = Math.ceil(instrumentNum / paginateBy);
+  const url = new URL(window.location.href);
+  const page = url.searchParams.get('page') || 1;
+  const paginateBy = url.searchParams.get('paginate_by') || 20;
+  setPage(page);
+  setPaginateBy(paginateBy);
+  pageNumInput.max = Math.ceil(instrumentNum / paginateBy);
 }
 
 function setPaginateBy(paginateBy) {
-    localStorage.setItem("paginate_by", paginateBy);
+  localStorage.setItem('paginate_by', paginateBy);
 }
 
 function setPage(pageNum) {
-    localStorage.setItem("page", pageNum);
+  localStorage.setItem('page', pageNum);
 }
 
 function getPaginateBy() {
-    return localStorage.getItem("paginate_by");
+  return localStorage.getItem('paginate_by');
 }
 
 function getPage() {
-    return localStorage.getItem("page");
+  return localStorage.getItem('page');
 }
 
-optionRadios.forEach(function(radio) {
-    if (radio.id == getPaginateBy()) {
-        radio.checked = true;   
-    }
+optionRadios.forEach(function (radio) {
+  if (radio.id == getPaginateBy()) {
+    radio.checked = true;
+  }
 });
 
-optionRadios.forEach(function(radio) {
-    radio.addEventListener("change", function() {
-        if (radio.checked) {
-            const selectedOptionInt = parseInt(radio.id);
-            setPaginateBy(selectedOptionInt);
-            maxPageNum = Math.ceil(instrumentNum / getPaginateBy());
-            validPageNum = Math.min(Math.max(getPage(), 1), maxPageNum);
-            if (validPageNum !== getPage()) {
-                setPage(validPageNum);
-            }
-            refreshPage();
-        }
-    });
-});
-
-pageNumInput.addEventListener("keydown", function(event) {
-    if (event.key === "Enter") {
-        maxPageNum = Math.ceil(instrumentNum / getPaginateBy());
-        validPageNum = Math.min(Math.max(pageNumInput.value, 1), maxPageNum);
-        pageNumInput.value = validPageNum;
-        pageNumInput.max = maxPageNum;
+optionRadios.forEach(function (radio) {
+  radio.addEventListener('change', function () {
+    if (radio.checked) {
+      const selectedOptionInt = parseInt(radio.id);
+      setPaginateBy(selectedOptionInt);
+      maxPageNum = Math.ceil(instrumentNum / getPaginateBy());
+      validPageNum = Math.min(Math.max(getPage(), 1), maxPageNum);
+      if (validPageNum !== getPage()) {
         setPage(validPageNum);
-        refreshPage();
+      }
+      refreshPage();
     }
+  });
+});
+
+pageNumInput.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    maxPageNum = Math.ceil(instrumentNum / getPaginateBy());
+    validPageNum = Math.min(Math.max(pageNumInput.value, 1), maxPageNum);
+    pageNumInput.value = validPageNum;
+    pageNumInput.max = maxPageNum;
+    setPage(validPageNum);
+    refreshPage();
+  }
 });
 
 pageBtns.forEach((btn) => {
-    btn.addEventListener("click", () => {
-        btn.href = btn.href + "&paginate_by=" + getPaginateBy();
-    });
+  btn.addEventListener('click', () => {
+    btn.href = btn.href + '&paginate_by=' + getPaginateBy();
+  });
 });
 
 function refreshPage() {
-    const url = new URL(window.location.href);
-    url.searchParams.set("page", getPage());
-    url.searchParams.set("paginate_by", getPaginateBy());
-    window.location.href = url.href;
+  const url = new URL(window.location.href);
+  url.searchParams.set('page', getPage());
+  url.searchParams.set('paginate_by', getPaginateBy());
+  window.location.href = url.href;
 }
