e9d974ee3fa66294ddda60813c2e4dac6e9f4ec6
Kun Fang <fangk3740@gmail.com>
2024-07-17 19:00:23 -0400

    fix: fix button bugs & optimize query
    - use "MIMO Classification" as a header in detail table in `detail.html`;
    - fix bugs of `Edit` buttons; delete uneccessary `Edit` buttons;
    - avoid duplicate query by using `context["instrument"]` instead of `get_object`;
    - get `active_language` for future feature of supporting English and French in detail page;

--- web-app/django/VIM/apps/instruments/views/instrument_detail.py
+++ web-app/django/VIM/apps/instruments/views/instrument_detail.py
@@ -1,5 +1,5 @@
 from django.views.generic import DetailView
-from VIM.apps.instruments.models import Instrument
+from VIM.apps.instruments.models import Instrument, Language
 
 
 class InstrumentDetail(DetailView):
@@ -13,33 +13,17 @@ class InstrumentDetail(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        instrument = self.get_object()
 
-        context["wikidata_id"] = instrument.wikidata_id
-        context["default_image_url"] = (
-            instrument.default_image.url if instrument.default_image else None
+        # Query the instrument names in all languages
+        context["instrument_names"] = (
+            context["instrument"].instrumentname_set.all().select_related("language")
         )
-        context["hornbostel_sachs_class"] = instrument.hornbostel_sachs_class
-        context["mimo_class"] = instrument.mimo_class
 
-        # Get english name
-        context["instrument_name_en"] = instrument.instrumentname_set.get(
-            language__en_label="english"
-        ).name
-
-        # Get all instrument names in all languages
-        instrument_names = []
-        for instrument_name in instrument.instrumentname_set.all():
-            instrument_names.append(
-                {
-                    "name": instrument_name.name,
-                    "source_name": instrument_name.source_name,
-                    "language_code": instrument_name.language.wikidata_code,
-                    "language_id": instrument_name.language.wikidata_id,
-                    "language_en_label": instrument_name.language.en_label,
-                    "language_autonym": instrument_name.language.autonym,
-                }
-            )
-        context["instrument_names"] = instrument_names
-
+        # Get the active language
+        active_language_en = self.request.session.get("active_language_en", None)
+        context["active_language"] = (
+            Language.objects.get(en_label=active_language_en)
+            if active_language_en
+            else Language.objects.get(en_label="english")  # default in English
+        )
         return context
