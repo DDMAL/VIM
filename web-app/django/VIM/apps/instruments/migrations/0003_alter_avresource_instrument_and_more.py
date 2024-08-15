56fef79f3b9c316e53e31643c9b0db78fe39efcd
Dylan Hillerbrand <dhillerbrand@gmail.com>
2024-08-15 08:12:15 -0400

    fix: format migration 0003 with black

--- web-app/django/VIM/apps/instruments/migrations/0003_alter_avresource_instrument_and_more.py
+++ web-app/django/VIM/apps/instruments/migrations/0003_alter_avresource_instrument_and_more.py
@@ -7,28 +7,46 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('instruments', '0002_instrument_thumbnail'),
+        ("instruments", "0002_instrument_thumbnail"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='avresource',
-            name='instrument',
-            field=models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='instruments.instrument'),
+            model_name="avresource",
+            name="instrument",
+            field=models.ForeignKey(
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to="instruments.instrument",
+            ),
         ),
         migrations.AlterField(
-            model_name='instrument',
-            name='default_image',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='default_image_of', to='instruments.avresource'),
+            model_name="instrument",
+            name="default_image",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="default_image_of",
+                to="instruments.avresource",
+            ),
         ),
         migrations.AlterField(
-            model_name='instrument',
-            name='thumbnail',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='thumbnail_of', to='instruments.avresource'),
+            model_name="instrument",
+            name="thumbnail",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="thumbnail_of",
+                to="instruments.avresource",
+            ),
         ),
         migrations.AlterField(
-            model_name='instrumentname',
-            name='instrument',
-            field=models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='instruments.instrument'),
+            model_name="instrumentname",
+            name="instrument",
+            field=models.ForeignKey(
+                on_delete=django.db.models.deletion.CASCADE, to="instruments.instrument"
+            ),
         ),
     ]
