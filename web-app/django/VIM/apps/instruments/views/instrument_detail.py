5b96da50ed04de4d8c609a03c11c59cc8b94dfc3
Zhanna Klimanova <zklimanova@yahoo.com>
2024-09-18 01:42:50 -0400

    feat: sort and search by language/name on edit page
    - Support sort by language
    - Support sort by name
    - Support search by language
    
    Refs: #137

--- web-app/django/VIM/apps/instruments/views/instrument_detail.py
+++ web-app/django/VIM/apps/instruments/views/instrument_detail.py
@@ -1,7 +1,6 @@
 from django.views.generic import DetailView
 from VIM.apps.instruments.models import Instrument, Language
 
-
 class InstrumentDetail(DetailView):
     """
     Displays details of a specific instrument.
@@ -14,10 +13,16 @@ class InstrumentDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        # Query the instrument names in all languages
-        context["instrument_names"] = (
-            context["instrument"].instrumentname_set.all().select_related("language")
-        )
+        search_query = self.request.GET.get('search', '')
+        sort_by = self.request.GET.get('sort', 'language__en_label')
+
+        instrument_names = context["instrument"].instrumentname_set.all().select_related("language")
+
+        if search_query:
+            instrument_names = instrument_names.filter(language__en_label__icontains=search_query)
+
+        instrument_names = instrument_names.order_by(sort_by)
+        context["instrument_names"] = instrument_names
 
         # Get the active language
         active_language_en = self.request.session.get("active_language_en", None)
